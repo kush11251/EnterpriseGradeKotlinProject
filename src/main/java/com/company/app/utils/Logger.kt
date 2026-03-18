9fea421f06f823e65aec09c612c873a6d79489d5
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2026-03-18 07:59:27 +0530

    feat: integrate src/main/java/com/company/app/utils/Logger.kt

---
+++ src/main/java/com/company/app/utils/Logger.kt
@@ -0,0 +1,10 @@
+
+            // Logger utility class
+            class Logger {
+                companion object {
+                    fun log(message: String) {
+                        println(message)
+                    }
+                }
+            }
+            
