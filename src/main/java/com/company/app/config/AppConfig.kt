8e4cfafbafb60934e362b564e2714b5508492c03
Kussagra Pathak <67318693+kush11251@users.noreply.github.com>
2026-03-18 07:59:18 +0530

    feat: integrate src/main/java/com/company/app/config/AppConfig.kt

---
+++ src/main/java/com/company/app/config/AppConfig.kt
@@ -0,0 +1,8 @@
+
+            // Application configuration class
+            class AppConfig {
+                fun getConfig(): String {
+                    return "Config loaded from env variables"
+                }
+            }
+            
